cfbda4c55e30dbff700097a90227085d4f975a40
Ahmet Eren Dinç <dincahmet87@gmail.com>
2023-03-25 00:54:45 +0300

    initial commit

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+         playerInputActions = new PlayerInputActions();
+         playerInputActions.Player.Enable();
+    }
+    public Vector2 GetMovementVectorNormalized()
+    {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+        inputVector = inputVector.normalized;
+        return inputVector;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameInput gameInput;
+
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir *moveSpeed * Time.deltaTime;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward ,  moveDir, Time.deltaTime * rotateSpeed);
+      
+
+
+    }
+}
