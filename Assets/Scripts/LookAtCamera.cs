dc191ebb0698fe37d24ffc5437794d05010b80cf
ttayfunylmz <ebruusayiil@gmail.com>
2023-04-09 15:33:36 +0300

    civil engineering npc added and connected to chatgpt

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    private enum Mode
+    {
+        LookAt,
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted,
+    }
+
+    [SerializeField] private Mode mode;
+
+    private void LateUpdate() 
+    {
+        switch(mode)
+        {
+            case Mode.LookAt:
+                transform.LookAt(Camera.main.transform);
+                break;
+            case Mode.LookAtInverted:
+                Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
+                transform.LookAt(transform.position + dirFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
+                break;
+        }
+    }
+}
